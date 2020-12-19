d93960c35509ed38971f2bc6d90b81ac65c874c1
MJ Rossetti <s2t2@users.noreply.github.com>
2020-12-19 18:34:23 -0500

    WIP - routing good

---
+++ NOTES.md
@@ -0,0 +1,5 @@
+# Stripe Reference Notes
+
+  + Payment Succeeds: 4242 4242 4242 4242
+  + Requires Auth: 4000 0025 0000 3155
+  + Payment Declined (client-side err): 4000 0000 0000 9995
--- web_app/routes/stripe_routes.py
+++ web_app/routes/stripe_routes.py
@@ -10,46 +10,61 @@ STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", default="OOPS")
 stripe.api_key = STRIPE_SECRET_KEY
 
 APP_DOMAIN = os.getenv("APP_DOMAIN", default="http://localhost:5000")
-SUCCESS_URL = f"{APP_DOMAIN}/stripe/callback/success.html"
-CANCEL_URL = f"{APP_DOMAIN}/stripe/callback/cancel.html"
-
-LINE_ITEMS = [
-    {
-        "price_data": {
-            "currency": "usd",
-            "unit_amount": 2_99,
-            "product_data": {"name": "Stubborn Attachments", "images": ["https://i.imgur.com/EHyR2nP.png"]},
-        },
-        "quantity": 1
-    }
-]
+SUCCESS_URL = f"{APP_DOMAIN}/stripe/callback/success"
+CANCEL_URL = f"{APP_DOMAIN}/stripe/callback/cancel"
 
 stripe_routes = Blueprint("stripe_routes", __name__)
 
-@stripe_routes.route("/stripe/checkout")
+@stripe_routes.route("/stripe/checkout-session/new")
 def checkout_page():
+    print("CHECKOUT PAGE")
     return render_template("stripe_checkout.html")
 
-@stripe_routes.route("/stripe/create-checkout-session", methods=["POST"])
+@stripe_routes.route("/stripe/checkout-session/create", methods=["POST"])
 def create_checkout_session():
-    print("CREATE CHECKOUT SESSION...")
+    """Launches a new stripe payment page with product info and credit card form """
+    print("CREATE CHECKOUT SESSION")
+    #print("FORM DATA:", dict(request.form))
+    #products = dict(request.form)
+
+    line_items = [
+        {
+            "price_data": {
+                "currency": "usd",
+                "unit_amount": 129_00,
+                "product_data": {"name": "My Book", "images": ["https://i.imgur.com/EHyR2nP.png"]},
+            },
+            "quantity": 1
+        }
+    ] # todo: pass selected products from the form data
 
     try:
         checkout_session = stripe.checkout.Session.create(
-            payment_method_types=["card"],
-            line_items=LINE_ITEMS,
-            mode="payment",
             success_url=SUCCESS_URL,
             cancel_url=CANCEL_URL,
+            mode="payment",
+            payment_method_types=["card"],
+            line_items=line_items,
+            #client_reference_id="", # A unique string to reference the Checkout Session. This can be a customer ID, a cart ID, or similar, and can be used to reconcile the session with your internal systems.
+            #customer_email="hello@example.com", # If provided, this value will be used when the Customer object is created. If not provided, customers will be asked to enter their email address.
+            #discount=[{"type": "coupon", "___": "____"}]
+            #submit_type="book", # "auto", "pay", "book", "donate"
         )
         return jsonify({"id": checkout_session.id})
     except Exception as e:
+        print("ERR:", e)
         return jsonify(error=str(e)), 403
 
 @stripe_routes.route("/stripe/callback/success")
 def callback_success():
-    return "SUCCESS!"
+    """Triggers after the user successfully enters their card info on the checkout page """
+    print("CALLBACK: SUCCESS")
+    flash("Payment Received. Thanks!", "success")
+    return redirect("/")
 
 @stripe_routes.route("/stripe/callback/cancel")
 def callback_cancel():
-    return "OOPS SOMETHING WENT WRONG!"
+    """Triggers if the user gets to the stripe checkout and then presses the back button there (not the browser back button) """
+    print("CALLBACK: CANCEL")
+    flash("Payment Canceled. It's OK.", "warning")
+    return redirect("/")
